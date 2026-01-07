7f7b201b543ec2ab7ca555093c3bc79e4f11b287
Jonh Henrique Moreira <107273593+JonhHenriqueMDC@users.noreply.github.com>
2026-01-07 00:55:24 -0300

    Update AppRouter.jsx

--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -1,15 +1,12 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home.jsx";
 import Timeline from "../pages/Timeline.jsx";
-import Ending from "../pages/Ending.jsx";
+
 
 export default function AppRouter() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/timeline" element={<Timeline />} />
-        <Route path="/ending" element={<Ending />} />
+        <Route path="/" element={<Timeline />} />
       </Routes>
     </HashRouter>
   );
